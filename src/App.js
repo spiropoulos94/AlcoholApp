7bc46a5412d99cdbc7f5135cb5037a6cba9e9526
Nikos Spiropoulos <spiropoulos94@gmail.com>
2020-08-23 19:04:01 +0300

    react Query

--- src/App.js
+++ src/App.js
@@ -3,28 +3,37 @@ import "./App.css";
 import Navbar from "./components/NavBar";
 import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
 import style from "./components/NavBar/styles.module.css";
+import { Button } from "@material-ui/core";
+import { ReactQueryDevtools } from "react-query-devtools";
+import { useQuery } from "react-query";
 
 function App() {
   return (
     <Router>
-      <div className="App-header">
-        <h1>Find a nice cocktail!</h1>
-      </div>
+      <div>
+        <div className="App-header">
+          <h1>Find a nice cocktail!</h1>
+        </div>
 
-      <div className={style.mainScreen}>
-        <h2>Choose your search method</h2>
-        <Navbar />
-        <Switch>
-          <Route path="/random">
-            <h2>Random Cocktail</h2>
-          </Route>
-          <Route path="/name">
-            <h2>Enter Name</h2>
-          </Route>
-          <Route path="/ingredients">
-            <h2>Enter Ingredients</h2>
-          </Route>
-        </Switch>
+        <div className={style.mainScreen}>
+          <h2>Choose your search method</h2>
+          <Navbar />
+          <Switch>
+            <Route path="/random">
+              <h2>Random Cocktail</h2>
+              <Button size="large" variant="contained" color="primary">
+                Random Cocktail!
+              </Button>
+            </Route>
+            <Route path="/name">
+              <h2>Enter Name</h2>
+            </Route>
+            <Route path="/ingredients">
+              <h2>Enter Ingredients</h2>
+            </Route>
+          </Switch>
+        </div>
+        <ReactQueryDevtools initialIsOpen />
       </div>
     </Router>
   );
