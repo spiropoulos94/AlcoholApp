7e24653aa50c550ac133fad81d0a703161da7c2a
Nikos Spiropoulos <spiropoulos94@gmail.com>
2020-08-23 17:32:17 +0300

    decided to use only material ui components
    As little customization as possible

--- src/components/NavBar/index.js
+++ src/components/NavBar/index.js
@@ -1,28 +1,36 @@
 import React from "react";
-import style from "./styles.module.css";
-import { NavLink } from "react-router-dom";
-import { Button } from "@material-ui/core";
+import { makeStyles } from "@material-ui/core/styles";
+import Paper from "@material-ui/core/Paper";
+import Tabs from "@material-ui/core/Tabs";
+import Tab from "@material-ui/core/Tab";
 
-function NavBar(props) {
+const useStyles = makeStyles({
+  root: {
+    flexGrow: 1,
+  },
+});
+
+export default function NavBar() {
+  const classes = useStyles();
+  const [value, setValue] = React.useState(0);
+
+  const handleChange = (event, newValue) => {
+    setValue(newValue);
+  };
+
   return (
-    <div className={style.nikos}>
-      <NavLink className={style.link} to="/random">
-        <Button variant="contained" color="primary">
-          Random Cocktail
-        </Button>
-      </NavLink>
-      <NavLink className={style.link} to="/name">
-        <Button variant="contained" color="primary">
-          Search by Name
-        </Button>
-      </NavLink>
-      <NavLink className={style.link} to="/ingredients">
-        <Button variant="contained" color="primary">
-          Search by ingredients
-        </Button>
-      </NavLink>
-    </div>
+    <Paper className={classes.root}>
+      <Tabs
+        value={value}
+        onChange={handleChange}
+        indicatorColor="primary"
+        textColor="primary"
+        centered
+      >
+        <Tab label="Item One" />
+        <Tab label="Item Two" />
+        <Tab label="Item Three" />
+      </Tabs>
+    </Paper>
   );
 }
-
-export default NavBar;
