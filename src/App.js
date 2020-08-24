fdbd93a0e5711280942ada488b0a48484e5a7ecf
Nikos Spiropoulos <spiropoulos94@gmail.com>
2020-08-24 18:49:37 +0300

    Update App.js

--- src/App.js
+++ src/App.js
@@ -3,17 +3,17 @@ import "./App.css";
 import Navbar from "./components/NavBar";
 import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
 import style from "./components/NavBar/styles.module.css";
-import { Button } from "@material-ui/core";
+import { Button, Container } from "@material-ui/core";
 import { ReactQueryDevtools } from "react-query-devtools";
-import { useQuery } from "react-query";
+//import { useQuery } from "react-query";
 
 function App() {
   return (
     <Router>
       <div>
-        <div className="App-header">
+        <Container fluid maxWidth="">
           <h1>Find a nice cocktail!</h1>
-        </div>
+        </Container>
 
         <div className={style.mainScreen}>
           <h2>Choose your search method</h2>
