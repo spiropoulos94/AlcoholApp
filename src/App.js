c3ce6f29801f71c5c5394af2c4157d961c247032
NikosSp <54745415+spiropoulos94@users.noreply.github.com>
2020-10-15 16:21:59 +0300

    removed second page. will implement a selection Page instead of a component

--- src/App.js
+++ src/App.js
@@ -19,23 +19,23 @@ function App() {
         <div class="App-header">
           <h1 class="welcome-text"> Find a nice cocktail! </h1>{" "}
         </div>{" "}
-        <div className={style.mainScreen}>
-          <h2> Choose your search method </h2> <Navbar />
-          <Switch>
-            <Route path="/random">
-              <h2> Random Cocktail </h2>{" "}
-              <Button size="large" variant="contained" color="primary">
-                Random Cocktail!
-              </Button>{" "}
-            </Route>{" "}
-            <Route path="/name">
-              <h2> Enter Name </h2>
-            </Route>{" "}
-            <Route path="/ingredients">
-              <h2> Enter Ingredients </h2>{" "}
-            </Route>{" "}
-          </Switch>{" "}
-        </div>{" "}
+        {/*<div className={style.mainScreen}>*/}
+        {/*  <h2> Choose your search method </h2> <Navbar />*/}
+        {/*  <Switch>*/}
+        {/*    <Route path="/random">*/}
+        {/*      <h2> Random Cocktail </h2>{" "}*/}
+        {/*      <Button size="large" variant="contained" color="primary">*/}
+        {/*        Random Cocktail!*/}
+        {/*      </Button>{" "}*/}
+        {/*    </Route>{" "}*/}
+        {/*    <Route path="/name">*/}
+        {/*      <h2> Enter Name </h2>*/}
+        {/*    </Route>{" "}*/}
+        {/*    <Route path="/ingredients">*/}
+        {/*      <h2> Enter Ingredients </h2>{" "}*/}
+        {/*    </Route>{" "}*/}
+        {/*  </Switch>{" "}*/}
+        {/*</div>{" "}*/}
         <ReactQueryDevtools initialIsOpen />
       </div>{" "}
     </Router>
