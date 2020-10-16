11b2fd8e2bcba847e1dd80b98007971b2e2cc886
NikosSp <54745415+spiropoulos94@users.noreply.github.com>
2020-10-16 12:52:56 +0300

    complete refactoring, dropped material ui for progress so far. might implemented later on

--- src/components/grid-item/grid-item.component.jsx
+++ src/components/grid-item/grid-item.component.jsx
@@ -1,14 +1,14 @@
 import React from 'react';
 import './grid-item.styles.scss';
+import {withRouter} from 'react-router-dom';
 
-const GridItem = ({title, icon, link}) => {
+const GridItem = ({title, icon, match}) => {
     return (
-        <div className="grid-item">
+        <div  className="grid-item">
             <span className="grid-item-title">{title}</span>
                 <img src={icon} width="40px;" style={{background: 'inherit'}} />
-
         </div>
     );
 };
 
-export default GridItem;
+export default withRouter(GridItem);
--- src/components/grid/grid.component.jsx
+++ src/components/grid/grid.component.jsx
@@ -1,17 +1,13 @@
 import React from 'react';
 import "./grid.styles.scss";
-import Button from "../button/button.component";
 import GridItem from "../grid-item/grid-item.component";
 
 const Grid = () => {
     return (
         <div className="grid">
-            {/*<Button>Method 1</Button>*/}
-            {/*<Button>Method 2</Button>*/}
-            {/*<Button>Method 3</Button>*/}
-            <GridItem icon="https://www.flaticon.com/svg/static/icons/svg/56/56936.svg" title='Search by Name'/>
-            <GridItem icon="https://www.flaticon.com/svg/static/icons/svg/1241/1241500.svg" title='Search by Ingredients' />
-            <GridItem icon="https://www.flaticon.com/svg/static/icons/svg/2776/2776157.svg" title="I feel lucky" />
+            <GridItem link="/name" icon="https://www.flaticon.com/svg/static/icons/svg/56/56936.svg" title='Search by Name'/>
+            <GridItem link="/ingredients" icon="https://www.flaticon.com/svg/static/icons/svg/1241/1241500.svg" title='Search by Ingredients' />
+            <GridItem link="/random" icon="https://www.flaticon.com/svg/static/icons/svg/2776/2776157.svg" title="I feel lucky" />
         </div>
     );
 };
