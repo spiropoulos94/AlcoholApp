f475e2ae08f2742d9fe17698bd764a192464bf54
Nikos Spiropoulos <spiropoulos94@gmail.com>
2020-10-17 19:56:36 +0300

    search-by-name-page.component

--- src/components/input/input.component.jsx
+++ src/components/input/input.component.jsx
@@ -1,13 +1,21 @@
-import React from 'react';
+import React,{useState} from 'react';
 import "./input.styles.scss";
 
 const Input = () => {
+
+    const [inputValue, setInputValue] = useState("")
+
+    function handleClick(e){
+        e.preventDefault();
+        console.log(inputValue)
+    }
+
     return (
-        <form className="input-component">
+        <div  className="input-component">
             <label htmlFor="cocktailSearch"/>
-            <input type="text" id="cocktailSearch" name="cocktailSearch" defaultValue="" />
-            <button className="input-button" type="submit">Search</button>
-        </form>
+            <input onChange={(e)=>setInputValue(e.target.value)} type="text" id="cocktailSearch" name="cocktailSearch" defaultValue={inputValue} />
+            <button onClick={(event)=>handleClick} className="input-button" >Search</button>
+        </div>
     );
 };
 
